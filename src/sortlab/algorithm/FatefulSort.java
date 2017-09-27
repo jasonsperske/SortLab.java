10d4d4ebf2e0266569b8b72c7c59492f89f52a3a
Jason Sperske <jason@sperske.com>
2017-09-26 21:11:24 -0700

    comment cleanup

--- src/sortlab/algorithm/FatefulSort.java
+++ src/sortlab/algorithm/FatefulSort.java
@@ -4,10 +4,11 @@ import java.util.Comparator;
 
 public final class FatefulSort<T> implements SortAlgorithmInterface<T> {
   private final Comparator<T> comparator;
+
   public FatefulSort(Comparator<T> comparator) {
-    //This is not a valid sorting algorithm. It was inspired by
+    //This is NOT a valid sorting algorithm. It was inspired by
     //https://xkcd.com/1185/ and will only work on data sets that (is if by fate)
-    //are already sorted. Becuase it relies so much on fate it does not include
+    //are already sorted. Because it relies so much on fate it does not include
     //any error checking (if you can't trust fate then what good is error
     //checking)
 
